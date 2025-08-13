1c354e01231ba407feba9141a592446a76010f06
Prashanth Puranik <puranik@digdeeper.in>
2025-08-13 14:27:16 +0530

    Step 10: Function type

--- demos/02-typescript/src/09-function.ts
+++ demos/02-typescript/src/09-function.ts
@@ -1,5 +1,6 @@
 type NS = number | string;
 
+// syntax 1
 // the data type of returned value is inferred
 function sum( x : NS, y : NS ) /*: number */ {
     // type narrowing
@@ -26,3 +27,27 @@ const result = sum( 1, 2 );
 sum( "1", 2 );
 sum( 1, "2" );
 sum( "1", "2" );
+
+// syntax 2 (expression-sytyle function defintions)
+type BinaryFunction = ( a : number, b : number ) => number;
+
+const subtract : BinaryFunction = function( x, y ) {
+    return x - y;
+};
+
+const multiply : BinaryFunction = ( x, y ) => x * y;
+
+// When we pass a function as an argument to another function (when we have callback functions)
+type AjaxCalbackFunction = ( result : string ) => void;
+
+function ajax( url : string, callback : AjaxCalbackFunction ) {
+    // ...
+}
+
+ajax(
+    'http://someurl.com/api/data',
+    function( result ) {
+        console.log( result );
+        // return 123;
+    }
+)
